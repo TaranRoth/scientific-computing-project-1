e8dd3188641a7b501af41413f95a8f92cd6c41c7
TaranRoth <taranroth1@gmail.com>
2025-11-19 11:46:20 -0500

    Plot legend and color coding for b

--- b.m
+++ b.m
@@ -19,10 +19,16 @@ rate_guess = 0.01;
 [r,final_error]=fminsearch(costfixed, ([1,1,1,1,rate_guess,rate_guess,rate_guess,rate_guess])');
 clf
 hold on
-plot(times,popdata,'Marker','v')
-plot(times, get_model(r,popdata), 'Marker', 'v')
-legend("Northeast","Midwest","South","West","Location","northwest")
-
+location_arr=["Northeast","Midwest","South","West"];
+popmodel = get_model(r,popdata);
+for j=1:4
+    p1 = plot(times,popdata(:,j),'Marker','v',"DisplayName", location_arr(j));
+    set(gca,'ColorOrderIndex',j)
+    p2 = plot(times, popmodel(:,j), 'Marker', 'o');
+    set(get(get(p2,'Annotation'),'LegendInformation'),'IconDisplayStyle','off'); 
+end
+legend("Location", "northwest")
+legend show
 hold off
 
 function y=cost(v, popdata)
@@ -33,8 +39,8 @@ end
 
 function y=get_model(v,popdata)
     A = [
-        (v(1) + 1) 0 -v(8) -v(7)
-        0 (v(2) + 1) -v(6) -v(5)
+        (v(1) + 1 - v(5) - v(7)) 0 0 0
+        0 (v(2) + 1 - v(6) - v(8)) 0 0
         v(5) v(6) (v(3) + 1) 0
         v(7) v(8) 0 (v(4) + 1)
         ];
